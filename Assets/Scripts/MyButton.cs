72bb3408a32e9b8278050f65be1696831487fb61
Mateusz Litwin <m_litwin@wp.pl>
2020-11-05 11:06:51 +0100

    Improved graphics

--- Assets/Scripts/MyButton.cs
+++ Assets/Scripts/MyButton.cs
@@ -4,11 +4,32 @@ using UnityEngine;
 
 public class MyButton : MonoBehaviour
 {
-    [SerializeField] GameObject blockade = default;
+    [SerializeField] GameObject[] blockade = default;
+    [SerializeField] Sprite[] usedLeverSprite = default;
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    bool used = false;
+    
+    void OnTriggerEnter2D(Collider2D collision)
     {
-        Destroy(blockade);
-        Destroy(gameObject);
+        if (!used)
+        {
+            foreach (var block in blockade)
+            {
+                Destroy(block);
+            }
+            if (gameObject.name == "Red Button")
+            {
+                GetComponent<SpriteRenderer>().sprite = usedLeverSprite[0];
+            }
+            else if (gameObject.name == "Green Button")
+            {
+                GetComponent<SpriteRenderer>().sprite = usedLeverSprite[1];
+            }
+            else
+            {
+                GetComponent<SpriteRenderer>().sprite = usedLeverSprite[2];
+            }
+            used = true;
+        }        
     }
 }
